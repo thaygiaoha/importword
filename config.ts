06de55651f97a4b27cca9a1023d516834488a644
thaygiaoha <thaygiaotoan6688@gmail.com>
2026-02-03 09:15:14 +0700

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,6 +1,7 @@
 
 import { Topic, ExamCodeDefinition, NewsItem, FixedConfig } from './types';
 export const DEFAULT_API_URL = "https://script.google.com/macros/s/AKfycbwZdb9kvZk6acp9aVkZBvsV-4hXpnQOdHYzOS1jHFmvwPsYcjXz7IDBQ8xuF_PgB3Bkkg/exec";
+export const SPREADSHEET_ID_ADMIN = "16w4EzHhTyS1CnTfJOWE7QQNM0o2mMQIqePpPK8TEYrg";
 
 export const DANHGIA_URL = DEFAULT_API_URL;
 // Khởi tạo rỗng, chúng ta sẽ lấp đầy nó sau khi App chạy
