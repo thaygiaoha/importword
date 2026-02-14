d4048c0fb2af9f88a46c9b8f005be4af1728786b
thaygiaoha <thaygiaotoan6688@gmail.com>
2026-02-14 16:43:26 +0700

    Update LandingPage.tsx

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -1357,8 +1357,7 @@ const handleRedirect = () => {
             </div>
           </div>
         )}
-      </div>
-    )}
+     
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
 
